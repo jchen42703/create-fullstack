30954b2fd68db59c922fdca31134a6c4b5176d04
Joseph Chen <jchen42703@gmail.com>
2022-12-10 06:36:23 -0500

    feat: broke TemplateGenerator up into separate generators

--- internal/generators/base.go
+++ internal/generators/base.go
@@ -1,5 +1,15 @@
 package generators
 
-type TemplateGenerator interface {
-	GenerateTemplate()
+import "github.com/jchen42703/create-fullstack/internal/configs"
+
+type FrontendTemplateGenerator interface {
+	GenerateTemplate(config configs.FrontendConfig)
+}
+
+type BackendTemplateGenerator interface {
+	GenerateTemplate(config configs.BackendConfig)
+}
+
+type FullstackTemplateGenerator interface {
+	GenerateTemplate(config configs.FullstackConfig)
 }
