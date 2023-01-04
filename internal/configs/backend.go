5bf0a56a0553756e99bc7256507a90f1a1c3c2bd
Joseph Chen <jchen42703@gmail.com>
2023-01-03 21:33:14 -0500

    feat(configs): matched BackendConfig with yaml

--- internal/configs/backend.go
+++ internal/configs/backend.go
@@ -1,17 +1,5 @@
 package configs
 
-type BackendBase int
-
-const (
-	Echo BackendBase = iota
-	Express
-	FastAPI
-)
-
-func (base BackendBase) String() string {
-	return []string{"Echo", "Express", "FastAPI"}[base]
-}
-
 type SQL_DB_TYPE int
 
 const (
@@ -38,20 +26,42 @@ func (base NO_SQL_DB_TYPE) String() string {
 }
 
 type SQLDBOptions struct {
-	DBType        SQL_DB_TYPE
-	StartupScript string // Runs this script to create the table etc.
+	DBType SQL_DB_TYPE `yaml:"db_type"`
+	// Runs this script to create the table etc.
+	StartupScript string `yaml:"startup_script"`
+}
+
+type PreCommitOptions struct {
+	Lint          bool `yaml:"lint"`
+	FormatterOpts *struct {
+		Formatter string
+	} `yaml:"format"`
+}
+
+type APIAugmentationOptions struct {
+	HuskyOpts     *HuskyOptions     `yaml:"husky"`
+	PreCommitOpts *PreCommitOptions `yaml:"pre_commit"`
+	AddDockerfile bool              `yaml:"dockerfile"`
+	GitOpts       *struct {
+		AddIssueTemplates bool `ymal:"issue_templates"`
+		AddPRTemplates    bool `yaml:"pr_templates"`
+	} `yaml:"git"`
+
+	AddCI string `yaml:"ci"`
 }
 
 type BackendConfig struct {
-	Base             BackendBase
-	DesiredImageName string
-	Databases        struct {
+	OutputDirectoryPath string                  `yaml:"output_dir"`
+	Base                string                  `yaml:"base"`
+	Language            string                  `yaml:"lang"`
+	AugmentOpts         *APIAugmentationOptions `yaml:"augment"`
+	Databases           struct {
 		// Assume one SQL database type.
 		// TODO: consider RDS/Spanner support
-		SQL *SQLDBOptions
+		SQL *SQLDBOptions `yaml:"sql"`
 
 		// Multiple NoSQL databases isn't uncommon.
 		// I.e. MongoDB as your regular DB + Redis for caching
-		NoSQL []NO_SQL_DB_TYPE
-	}
+		NoSQL []NO_SQL_DB_TYPE `yaml:"no_sql"`
+	} `yaml:"db"`
 }
