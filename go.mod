83816c88098f4d8233b61b247cbdd94b8ea3d069
Joseph Chen <jchen42703@gmail.com>
2023-01-04 00:59:30 -0500

    feat: added zap as logger

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/agext/levenshtein v1.2.3
 	github.com/hashicorp/go-version v1.6.0
 	github.com/mitchellh/cli v1.1.5
+	go.uber.org/zap v1.24.0
 )
 
 require (
@@ -27,6 +28,8 @@ require (
 	github.com/posener/complete v1.1.1 // indirect
 	github.com/shopspring/decimal v1.2.0 // indirect
 	github.com/spf13/cast v1.3.1 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/multierr v1.9.0 // indirect
 	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
 	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
 )
---
+++ internal/log/base.go
@@ -0,0 +1,59 @@
+package log
+
+import (
+	"fmt"
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+// Sets the logger level based on the environment variable.
+// Completely ignore if no env variable set.
+func SetLoggerLevel(envVarName string, atom zap.AtomicLevel) error {
+	lvl := os.Getenv(envVarName)
+	// Expects the string representation of log levels
+	// I.e. debug, warn, info, error, panic, fatal
+	logLvl, err := zapcore.ParseLevel(lvl)
+	// Just exit early with error
+	if err != nil {
+		return err
+	}
+
+	atom.SetLevel(logLvl)
+	return nil
+}
+
+// Creates a logger instance, the logger file, and sets the logger level based on the
+// CREATE_FULLSTACK_LOG_LVL environment.
+// The logger logs to both a logger file (DEBUG level) and stdout (Info).
+func CreateLogger(logFilePath string) (*zap.Logger, error) {
+	// Create logger
+	atom := zap.NewAtomicLevel()
+
+	// To keep the example deterministic, disable timestamps in the output.
+	encoderCfg := zap.NewProductionEncoderConfig()
+
+	// Creates log file if does not already exist
+	f, err := os.Create(logFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("CreateLogger: %s", err.Error())
+	}
+
+	// File + JSON logger
+	core := zapcore.NewTee(
+		zapcore.NewCore(zapcore.NewJSONEncoder(encoderCfg), zapcore.AddSync(f), zap.DebugLevel),
+		zapcore.NewCore(
+			zapcore.NewJSONEncoder(encoderCfg),
+			zapcore.Lock(os.Stdout),
+			atom,
+		))
+
+	logger := zap.New(core)
+	err = SetLoggerLevel("CREATE_FULLSTACK_LOG_LVL", atom)
+	if err != nil {
+		logger.Debug("using default logger level: WARN")
+	}
+
+	return logger, nil
+}
