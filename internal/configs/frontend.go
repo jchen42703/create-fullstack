1083aea29b26425c2c397cc993b356a1f2e17728
Joseph Chen <jchen42703@gmail.com>
2023-01-03 22:46:40 -0500

    feat: created PackageOptions and added to FrontendConfig

--- internal/configs/frontend.go
+++ internal/configs/frontend.go
@@ -1,27 +1,29 @@
 package configs
 
-type FrontendBase int
-
-const (
-	Next FrontendBase = iota
-	CRA
-)
-
-func (base FrontendBase) String() string {
-	return []string{"Next.js", "CRA"}[base]
+type HuskyOptions struct {
+	CommitLint *PackageOptions `yaml:"commitlint"`
+	// Might need to change these to include specific formatters/linters and their versions
+	Format bool `yaml:"format"`
+	Lint   bool `yaml:"lint"`
 }
 
-type HuskyOptions struct {
-	CommitLint bool
-	Prettier   bool
-	ESLint     bool
+type FrontendAugmentOptions struct {
+	AddSCSS             *PackageOptions `yaml:"scss"`
+	AddTailwind         *PackageOptions `yaml:"tailwind"`
+	AddStyledComponents *PackageOptions `yaml:"styled_components"`
+	HuskyOpts           *HuskyOptions   `yaml:"husky"`
+	AddDockerfile       bool            `yaml:"dockerfile"`
+	GitOpts             *struct {
+		AddIssueTemplates bool `ymal:"issue_templates"`
+		AddPRTemplates    bool `yaml:"pr_templates"`
+	} `yaml:"git"`
+
+	AddCI string `yaml:"ci"`
 }
 
 type FrontendConfig struct {
-	Base                FrontendBase
-	UseTypescript       bool
-	UseSCSS             bool
-	UseTailwind         bool
-	UseStyledComponents bool
-	HuskyOpts           *HuskyOptions
+	OutputDirectoryPath string                  `yaml:"output_dir"`
+	Base                string                  `yaml:"base"`
+	Language            string                  `yaml:"lang"`
+	AugmentOpts         *FrontendAugmentOptions `yaml:"augment"`
 }
---
+++ internal/configs/version.go
@@ -0,0 +1,7 @@
+package configs
+
+// Options for when you add a package.
+// This exists to make it easy to manage dependencies across the CLI.
+type PackageOptions struct {
+	Version string
+}
