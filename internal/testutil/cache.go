79edcdfe5af44ca8ea0251b6405c3f3b3562c9cc
Joseph Chen <jchen42703@gmail.com>
2023-01-06 01:56:51 -0500

    feat: cached templates in BaseTemplateCache

---
+++ internal/testutil/cache.go
@@ -0,0 +1,46 @@
+package testutil
+
+import (
+	"fmt"
+	"os"
+	"sync"
+
+	"github.com/jchen42703/create-fullstack/internal/directory"
+)
+
+type BaseTemplateCache struct {
+	mu            sync.RWMutex
+	TemplatePaths map[string]string
+}
+
+func NewBaseTemplateCache() *BaseTemplateCache {
+	return &BaseTemplateCache{
+		TemplatePaths: map[string]string{},
+	}
+}
+
+func (c *BaseTemplateCache) GetTemplateAndCopy(selectedTemplate, outputDir string) error {
+	defer c.mu.Unlock()
+	c.mu.Lock()
+	cachedTemplatePath, ok := c.TemplatePaths[selectedTemplate]
+	if !ok {
+		return fmt.Errorf("template %s not found in cache", selectedTemplate)
+	}
+
+	// Copy to output dir
+	if err := os.MkdirAll(outputDir, 0777); err != nil {
+		return fmt.Errorf("failed to create outputDir: %s", err)
+	}
+
+	if err := directory.CopyDirectory(cachedTemplatePath, outputDir); err != nil {
+		return fmt.Errorf("GetTemplateAndCopy: failed to copy to outputDir: %s\n", err)
+	}
+
+	return nil
+}
+
+func (c *BaseTemplateCache) AddTemplate(templateName, templatePath string) {
+	defer c.mu.Unlock()
+	c.mu.Lock()
+	c.TemplatePaths[templateName] = templatePath
+}
