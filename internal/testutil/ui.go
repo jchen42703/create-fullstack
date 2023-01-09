c139db423435ff0e4229835eba6d1df22cd90964
Joseph Chen <jchen42703@gmail.com>
2023-01-09 16:56:38 -0500

    feat: added CreateTestDirAndLogger and CleanupBaseTest

--- internal/testutil/ui.go
+++ internal/testutil/ui.go
@@ -1,34 +1,54 @@
 package testutil
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
-	"testing"
 
+	"github.com/jchen42703/create-fullstack/internal/directory"
+	"github.com/jchen42703/create-fullstack/internal/log"
 	"go.uber.org/zap"
 )
 
-// General cleanup function that removes an output directory, and removes the logger file.
-func CleanupUiTest(t *testing.T, outputDir string, logFilePath string, logger *zap.Logger) {
-	err := os.Chdir("../")
+// Creates test directory and logger.
+// Returns the logger, test working directory path, or error
+func CreateTestDirAndLogger(testDir string) (*zap.Logger, string, error) {
+	// Create test files and test in a separate directory.
+	wd, err := os.Getwd()
 	if err != nil {
-		t.Fatalf("failed to change back to reg directory during cleanup: %s", err)
+		return nil, "", fmt.Errorf("failed to Getwd: %s", err)
 	}
 
-	err = os.RemoveAll(outputDir)
+	// Create test working dir + logger
+	testWd := filepath.Join(wd, testDir)
+	err = os.Mkdir(testWd, directory.READ_WRITE_EXEC_PERM)
 	if err != nil {
-		t.Fatalf("failed to clean up test directory: %s", err)
+		return nil, "", fmt.Errorf("failed to mk test dir: %s", err)
 	}
 
-	err = os.Remove(logFilePath)
+	logFilePath := filepath.Join(testWd, "create-fullstack.log")
+	logger, err := log.CreateLogger(logFilePath)
 	if err != nil {
-		t.Fatalf("failed to cleanup log: %s", err)
+		return nil, "", fmt.Errorf("failed to create logger")
+	}
+
+	return logger, testWd, nil
+}
+
+// Generic test to cleanup the working directory and logger.
+func CleanupBaseTest(testWd string, logger *zap.Logger) error {
+	err := os.RemoveAll(testWd)
+	if err != nil {
+		return fmt.Errorf("failed to cleanup log: %s", err)
 	}
 
 	if err := logger.Sync(); err != nil {
 		// this sync error is safe to ignore, since stdout doesn't support syncing in Linux/OS X
 		if !strings.HasSuffix(err.Error(), "sync /dev/stdout: invalid argument") {
-			t.Fatalf("Error cleaning up logger: %s", err.Error())
+			return fmt.Errorf("Error cleaning up logger: %s", err.Error())
 		}
 	}
+
+	return nil
 }
