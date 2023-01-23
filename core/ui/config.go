de95975ea070ac0201cc7fa1b505718f50f66cfa
Benson <36316652+Jinb2@users.noreply.github.com>
2023-01-22 19:13:40 -0500

    feat(validator): added validator tags to structs

--- core/ui/config.go
+++ core/ui/config.go
@@ -8,22 +8,22 @@ import (
 type HuskyOptions struct {
 	CommitLint *opts.PackageOptions `yaml:"commitlint"`
 	// Might need to change these to include specific formatters/linters and their versions
-	Format bool `yaml:"format"`
-	Lint   bool `yaml:"lint"`
+	Format bool `yaml:"format" validate:"required,boolean"`
+	Lint   bool `yaml:"lint" validate:"required,boolean"`
 }
 
 type UiAugmentOptions struct {
-	AddScss             *opts.PackageOptions `yaml:"scss"`
-	AddTailwind         *opts.PackageOptions `yaml:"tailwind"`
-	AddStyledComponents *opts.PackageOptions `yaml:"styled_components"`
-	HuskyOpts           *HuskyOptions        `yaml:"husky"`
-	AddDockerfile       bool                 `yaml:"dockerfile"`
+	AddScss             *opts.PackageOptions `yaml:"scss" validate:"required"`
+	AddTailwind         *opts.PackageOptions `yaml:"tailwind" validate:"required"`
+	AddStyledComponents *opts.PackageOptions `yaml:"styled_components" validate:"required"`
+	HuskyOpts           *HuskyOptions        `yaml:"husky" validate:"required"`
+	AddDockerfile       bool                 `yaml:"dockerfile" validate:"required,boolean"`
 	GitOpts             *struct {
-		AddIssueTemplates bool `ymal:"issue_templates"`
-		AddPRTemplates    bool `yaml:"pr_templates"`
-	} `yaml:"git"`
+		AddIssueTemplates bool `yaml:"issue_templates" validate:"required,boolean"`
+		AddPRTemplates    bool `yaml:"pr_templates" validate:"required,boolean"`
+	} `yaml:"git" validate:"required"`
 
-	AddCi string `yaml:"ci"`
+	AddCi string `yaml:"ci" validate:"required,oneof=circleci travisci jenkins git_workflows"`
 }
 
 type TemplateConfig struct {
