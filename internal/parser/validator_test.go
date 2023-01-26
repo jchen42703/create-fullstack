157f2033ca4e55b7614acc2b6dd2735b08f0b466
Benson <36316652+Jinb2@users.noreply.github.com>
2023-01-25 20:35:44 -0500

    feat(validator): added more tests for the validator function

--- internal/parser/validator_test.go
+++ internal/parser/validator_test.go
@@ -3,16 +3,48 @@ package parser_test
 import (
 	"testing"
 
+	"github.com/jchen42703/create-fullstack/core/opts"
+	"github.com/jchen42703/create-fullstack/core/ui"
 	"github.com/jchen42703/create-fullstack/internal/parser"
 )
 
 func TestValidator(t *testing.T) {
 
-	tests := []parser.TemplateConfig {
+	tests := map[string]parser.TemplateConfig{
 
-		{OutputDirectoryPath: "asdsad", 
-		Base: "asas", 
-		Language: "typescript",
-		AugmentOpts: &ui.UiAugmentOptions{}}
+		"ValidDirectoryPath": {
+			OutputDirectoryPath: "C:\\Users\\19172\\create-fullstack\\internal\\parser",
+			Base:                "asas",
+			Language:            "typescript",
+			AugmentOpts:         &ui.UiAugmentOptions{},
+		},
+		"NoAugmentOpts": {
+			OutputDirectoryPath: "asdsad",
+			Base:                "asas",
+			Language:            "typescript",
+			AugmentOpts:         &ui.UiAugmentOptions{},
+		},
+		"ValidConfigMissingGitOpts": {
+			OutputDirectoryPath: "C:\\Users\\19172\\create-fullstack\\internal\\parser",
+			Base:                "asas",
+			Language:            "typescript",
+			AugmentOpts: &ui.UiAugmentOptions{
+				AddScss:             &opts.PackageOptions{Version: "test"},
+				AddTailwind:         &opts.PackageOptions{Version: "test"},
+				AddStyledComponents: &opts.PackageOptions{Version: "test"},
+				HuskyOpts:           &ui.HuskyOptions{Format: true, Lint: true},
+				AddDockerfile:       true,
+				AddCi:               "jenkins",
+			},
+		},
 	}
+
+	for name, testEx := range tests {
+
+		if err := testEx.Validate(); err != nil {
+			t.Fatalf("Example %s is not a valid config because %s", name, err)
+		}
+
+	}
+
 }
